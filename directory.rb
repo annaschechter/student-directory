7794e557e749fac0c96d0e8aab7e71c272c7c479
Anna Jerofejeva <annajerofejeva@Annas-Mac.local>
2014-09-07 13:10:16 -0700

    Asking for user input

--- directory.rb
+++ directory.rb
@@ -1,14 +1,22 @@
-students = [
-{:name => "Nina Herring", :cohort => :september},
-{:name => "Sam Becker", :cohort => :september},
-{:name => "Sarah Lester", :cohort => :september},
-{:name => "Mario Young", :cohort => :september},
-{:name => "Ayse Barch", :cohort => :september},
-{:name => "Linda Cooper", :cohort => :september},
-{:name => "Deborah Jordan", :cohort => :september},
-{:name => "Lucas Scott", :cohort => :september},
-{:name => "Andrew Oates", :cohort => :september}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create am empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+  #add the student hash to the array
+  students << {:name => name, :cohort => :september}
+  puts "Now we have #{students.length} students"
+  # get another name from the user
+  name = gets.chomp	
+  end
+  # return the array of students
+  students	
+  end
+
 
 def print_header
   puts "The students of my cohort at Makers Academy"
@@ -24,6 +32,7 @@ def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
