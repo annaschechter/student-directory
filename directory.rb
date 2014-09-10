a57ee6efee1e76651f9d3f724af85b38c725e9f5
Anna Jerofejeva <annajerofejeva@Annas-Mac.local>
2014-09-10 06:37:53 -0700

    Refactored the code agai

--- directory.rb
+++ directory.rb
@@ -1,78 +1,100 @@
+@students = []
+
 def interactive_menu
-  students = []
-  # 4. repeat from step 1
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. input the students"
-    puts "2. show the students"
-    puts "9. exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-      when "1"
-        # input the students
-        students = input_students
-      when "2"
-        # show the students
-        printing_header
-        printing(students)
-        printing_footer(students)
-      when "9"
-        exit # this will cause the program to terminate
-      else 
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
-  
-def input_students
-  print "Please enter the name of the student\n".center(50)
-  #create am empty array
-  students = []
-  #get the first name
-  name = gets.chomp
-  #while the name is not empty, repeat this code
-  while !name.empty?
-    print "Please enter the cohort of the student\n".center(50)
-    #get the cohort
-    cohort = gets.chomp
-    months = [:september, :october, :november, :december, :january, :february, :march, :april, :may, :june, :july, :august]
-    while !months.include?(cohort.downcase.to_sym)
-      print "Cohort does not exist, please check the spelling\n".center(50)
-      cohort = gets.chomp
-    end
-    print "Please enter the student's country of birth\n".center(50)
-    #get the country
-    country = gets.chomp
-    while country.empty?
-      print "Please enter the student's country of birth\n".center(50)
-      #get the country
-      country = gets.chomp
-    end
-    print "Please enter the student's hobby\n".center(50)
-    #get the hobby
-    hobby = gets.chomp
-    while hobby.empty? 
-      print "Please enter the student's hobby\n".center(50)
-      #get the hobby
-      hobby = gets.chomp
-    end
-    #add the student hash to the array
-    students << {:name => name.capitalize, :cohort => cohort.downcase.to_sym, :country => country, :hobby => hobby}
-    if students.length == 1
-      print "Now we have 1 student\n"
-    else
-      print "Now we have #{students.length} students\n"
-    end
-    # get another name from the user
-    print "Please enter the name of the next student\n".center(50)
-    print "To finish, just hit return\n".center(50)
-    name = gets.chomp
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else 
+      puts "I don't know what you mean, try again"
   end
-  # return the array of students
-  students  
+end
+
+
+def show_students
+  printing_header
+  print_student_list
+  printing_footer
+end
+
+
+def input_students
+  get_the_name
+  #while the name is not empty, repeat this code
+  while !@name.empty?
+  get_the_cohort
+  get_the_country
+  get_the_hobby
+  add_to_the_array
+  get_another_name
+  end
+  @students
+end
+
+def get_the_name
+  print "Please enter the name of the student\n".center(50)
+  @name = gets.chomp
+end
+
+def get_the_cohort
+  print "Please enter the cohort of the student\n".center(50)
+  @cohort = gets.chomp
+  @months = [:september, :october, :november, :december, :january, :february, :march, :april, :may, :june, :july, :august]
+  while !@months.include?(@cohort.downcase.to_sym)
+    print "Cohort does not exist, please check the spelling\n".center(50)
+    @cohort = gets.chomp
+  end
+end
+
+def get_the_country    
+  print "Please enter the student's country of birth\n".center(50)
+  @country = gets.chomp
+  while @country.empty?
+    print "Please enter the student's country of birth\n".center(50)
+    @country = gets.chomp
+  end
+end
+
+def get_the_hobby
+  print "Please enter the student's hobby\n".center(50)
+  @hobby = gets.chomp
+  while @hobby.empty? 
+    print "Please enter the student's hobby\n".center(50)
+    @hobby = gets.chomp
+  end
+end
+
+def add_to_the_array
+  @students << {:name => @name.capitalize, :cohort => @cohort.downcase.to_sym, :country => @country, :hobby => @hobby}
+  if @students.length == 1
+    print "Now we have 1 student\n"
+  else
+    print "Now we have #{@students.length} students\n"
+  end
+end
+ 
+def get_another_name
+  print "Please enter the name of the next student\n".center(50)
+  print "To finish, just hit return\n".center(50)
+  @name = gets.chomp
 end
 
 
@@ -82,26 +104,25 @@ def printing_header
 end
 
 
-def printing(names)
-  months = [:september, :october, :november, :december, :january, :february, :march, :april, :may, :june, :july, :august]
+def print_student_list
   num = 0
-  while num < months.length
-    x = names.select{|student| student[:cohort] == months[num]}
+  while num < @months.length
+    x = @students.select{|student| student[:cohort] == @months[num]}
     if !x.empty?
-    print "#{months[num].capitalize}:\n"
-    x.each {|student| print "#{student[:name].capitalize} is from #{student[:country].capitalize} and their favoutite hobby is #{student[:hobby]}\n"}
+    print "#{@months[num].capitalize}:\n"
+    x.each {|student| print "#{student[:name].capitalize} is from #{student[:country].capitalize} and #{student[:name].capitalize}'s favourite hobby is #{student[:hobby]}\n"}
     end
     num += 1
   end 
 end
 
 
-def printing_footer(names)
-  names.each 
-  if names.length == 1 
+def printing_footer
+  @students.each 
+  if @students.length == 1 
     print "Overall, we have 1 great student\n" 
   else
-    print "Overall, we have #{names.length} great students\n"
+    print "Overall, we have #{@students.length} great students\n"
   end
 end
 
